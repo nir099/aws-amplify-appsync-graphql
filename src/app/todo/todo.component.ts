c7373097317e50c2d5d5cc4300005b079cc33cf3
nir099 <a.m.s.nirmal.a@gmail.com>
2019-10-07 11:57:29 +0530

    complete app

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,3 +1,4 @@
+import { APIService } from './../API.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  allTodos: any = [];
 
-  ngOnInit() {
+  constructor(private api: APIService) { }
+
+  async ngOnInit() {
+    const  result = await this.api.ListTodos();
+    this.allTodos = result.items;
   }
 
+  async createTodo() {
+    const newTodo = {
+      name: 'Todo' + Math.floor(Math.random() + 100),
+      description: 'this is a todo',
+      completed: false
+    };
+
+    const result = await this.api.CreateTodo(newTodo);
+    this.allTodos.push(result);
+  }
+
+  async listTodos() {
+    const  result = await this.api.ListTodos();
+    this.allTodos = result.items;
+  }
 }
